8eb8b14824414578724ceb47423b8bc6480fb0e9
dlamb01 <81256865+dlamb01@users.noreply.github.com>
2026-03-31 09:25:21 -0400

    add bottom nav

--- app/src/main/java/com/cs388group/refrigeratormanager/MainActivity.kt
+++ app/src/main/java/com/cs388group/refrigeratormanager/MainActivity.kt
@@ -4,9 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.cs388group.refrigeratormanager.databinding.ActivityMainBinding
+import com.cs388group.refrigeratormanager.fragments.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -17,15 +18,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
+        /*
         binding.btnScan.setOnClickListener {
             val intent = Intent(this, BarcodeScannerActivity::class.java)
             startActivity(intent)
         }
+         */
+
+        val homeFragment = HomeFragment()
+        val scanFragment = ScanFragment()
+        val genAiFragment = GenAiFragment()
+        val settingsFragment = SettingsFragment()
+
+
+
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.menu_item_home -> fragment = homeFragment
+                R.id.menu_item_scan -> fragment = scanFragment
+                R.id.menu_item_genai -> fragment = genAiFragment
+                R.id.menu_item_settings -> fragment = settingsFragment
+            }
+            replaceFragment(fragment)
+            true
+        }
+
+        bottomNavigationView.selectedItemId = R.id.menu_item_home
+        replaceFragment(HomeFragment())
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frame_layout, fragment)
+            .commit()
     }
 }
